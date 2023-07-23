dd9bfe4aa90b9e458ca661085399d49062d8df60
Varshithvhegde <varshithvh@gmail.com>
2023-07-23 10:43:52 +0530

    Google Analytics added

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import ReactGA from "react-ga4";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactGA.initialize("G-ZP86N39D3T");
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
+const SendAnalytics = ()=> {
+  ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+  });
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(SendAnalytics);
